ed49da3d1b3bdb5e09bf5dbeac87586026d11400
HetulM <hetul834@gmail.com>
2021-03-06 01:26:03 +0000

    Adding job details and company information to the card. Added a button to view the job details.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
 import Job from './Job';
+import JobsPagination from './jobsPagination';
 
 function App() {
   const [params, setParams] = useState({});
@@ -9,12 +10,15 @@ function App() {
   const { jobs, loading, error } = useFetchJobs(params, page);
 
   return (
-    <Container>
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
+      <JobsPagination page={page} setPage={setPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try refreshing.</h1>}
       {jobs.map((job) => {
         return <Job key={job.id} job={job} />;
       })}
+      <JobsPagination page={page} setPage={setPage} />
     </Container>
   );
 }
