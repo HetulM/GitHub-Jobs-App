d3e2188d7304fd6ebe03f54352b805e3d4a2a8d6
HetulM <hetul834@gmail.com>
2021-03-07 15:25:04 +0000

    Adding form to filter search results.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
 import Job from './Job';
 import JobsPagination from './jobsPagination';
+import SearchForm from './SearchForm';
 
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
+  // 'e' = event
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  }
+
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try refreshing.</h1>}
